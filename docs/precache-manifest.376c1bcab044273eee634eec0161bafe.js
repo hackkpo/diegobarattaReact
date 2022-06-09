858a4fb3a5081014e7ef7665710a245123cfc017
Diego Baratta <diego.f.baratta@gmail.com>
2022-06-09 19:21:57 -0300

    Deply githubpages 2.0

---
+++ docs/precache-manifest.376c1bcab044273eee634eec0161bafe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e25bf48ec0f0fd10ffb9",
+    "url": "/static/css/main.23a708fc.chunk.css"
+  },
+  {
+    "revision": "e25bf48ec0f0fd10ffb9",
+    "url": "/static/js/main.e25bf48e.chunk.js"
+  },
+  {
+    "revision": "14b060c7915162ae1f5f",
+    "url": "/static/js/1.14b060c7.chunk.js"
+  },
+  {
+    "revision": "7164967f06212c480270",
+    "url": "/static/js/2.7164967f.chunk.js"
+  },
+  {
+    "revision": "c8dbfd972bc17939af0e",
+    "url": "/static/js/runtime~main.c8dbfd97.js"
+  },
+  {
+    "revision": "63f3b930a01553fb7e505ea0217d7768",
+    "url": "/index.html"
+  }
+];
